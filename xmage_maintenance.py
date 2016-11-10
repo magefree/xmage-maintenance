95f6780ea9fada8bd141705e8923790b93d6332a
Fenhl <fenhl@fenhl.net>
2016-11-10 23:20:54 +0000

    Add `implemented-since` subcommand, closes #1

--- xmage_maintenance.py
+++ xmage_maintenance.py
@@ -5,17 +5,18 @@
 Usage:
   xmage-maintenance [options] full-spoiler <set_code> <spoiler_url>
   xmage-maintenance [options] implemented <card_name> [<set_code>]
+  xmage-maintenance [options] implemented-since <revision>
   xmage-maintenance [options] markdown-link <card_name> <set_code>
   xmage-maintenance [options] oracle-update <set_code>
   xmage-maintenance [options] total
   xmage-maintenance -h | --help
 
 Options:
-  -h, --help  Print this message and exit.
+  -h, --help     Print this message and exit.
+  -p, --pull     Pull master before performing maintenance.
+  -v, --verbose  Print progress updates while performing maintenance.
   --patch     When used with the `oracle-update` subcommand, only copy the cards section.
-  --pull      Pull master before performing maintenance.
   --stdout    Print to stdout instead of copying to clipboard.
-  --verbose   Print progress updates while performing maintenance.
 """
 
 import sys
@@ -69,9 +70,10 @@ def copy(text):
     else:
         subprocess.run(['pbcopy'], input=text.encode('utf-8'))
 
-def implemented(name, expansion=None):
-    candidates = (MASTER / 'Mage.Sets' / 'src' / 'mage' / 'sets').iterdir()
-    for set_class in candidates:
+def implemented(name, expansion=None, *, repo=MASTER):
+    for set_class in (repo / 'Mage.Sets' / 'src' / 'mage' / 'sets').iterdir():
+        if set_class.is_dir():
+            continue
         with set_class.open() as f:
             text = f.read()
         for line in text.split('\n'):
@@ -84,11 +86,42 @@ def implemented(name, expansion=None):
                 return True
     return False
 
+def iter_implemented(*, repo=MASTER):
+    for set_class in (repo / 'Mage.Sets' / 'src' / 'mage' / 'sets').iterdir():
+        if set_class.is_dir():
+            continue
+        set_code = None
+        with set_class.open() as f:
+            text = f.read()
+        for line in text.split('\n'):
+            if set_code is None:
+                match = re.match('        super\\("[^"]+", "([A-Z0-9]+)"', line)
+                if match:
+                    set_code = match.group(1)
+            else:
+                match = re.search('cards.add\\(new SetCardInfo\\("([^"]+)",', line)
+                if match:
+                    yield set_code, match.group(1)
+
 def markdown_card_link(name, set_code, db=None):
     if db is None:
         db = mtgjson.CardDb.from_url()
-    card = db.sets[set_code].cards_by_name[name]
-    return '[{}](https://mtg.wtf/card/{}/{})'.format(name, set_code.lower(), card.number)
+    try:
+        card = db.sets[set_code].cards_by_name[name]
+    except KeyError:
+        return name
+    try:
+        url_set_code = db.sets[set_code].magicCardsInfoCode
+    except AttributeError:
+        url_set_code = set_code
+    try:
+        number = card.number
+    except AttributeError:
+        try:
+            number = card.mciNumber
+        except AttributeError as e:
+            return '[{}](https://mtg.wtf/card?q=%21{})'.format(name, name.replace(' ', '+'))
+    return '[{}](https://mtg.wtf/card/{}/{})'.format(name, url_set_code, number)
 
 if __name__ == '__main__':
     arguments = docopt.docopt(__doc__)
@@ -147,6 +180,44 @@ if __name__ == '__main__':
         if OPTIONS['verbose']:
             print('[{}] {}{}'.format(' ok ' if impl else 'FAIL', '({}) '.format(arguments['<set_code>']) if arguments['<set_code>'] else '', arguments['<card_name>']))
         sys.exit(0 if impl else 1)
+    elif arguments['implemented-since']:
+        try:
+            if OPTIONS['verbose']:
+                print('[....] downloading MTG JSON', end='', flush=True)
+            db = mtgjson.CardDb.from_url()
+            if OPTIONS['verbose']:
+                print('\r[ ok ]')
+                print('[....] determining current implemented cards', end='', flush=True)
+            current_implemented = collections.defaultdict(set)
+            for set_code, card_name in iter_implemented():
+                current_implemented[set_code].add(card_name)
+            if OPTIONS['verbose']:
+                print('\r[ ok ]')
+                print('[....] determining implemented cards as of given revision', end='', flush=True)
+            subprocess.check_call(['git', 'checkout', arguments['<revision>']], cwd=str(MASTER), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            for set_code, card_name in iter_implemented():
+                current_implemented[set_code].discard(card_name)
+            if OPTIONS['verbose']:
+                print('\r[ ok ]')
+                print('[....] formatting', end='', flush=True)
+            output = []
+            for i, (set_code, set_implemented) in enumerate(sorted(current_implemented.items())):
+                if OPTIONS['verbose']:
+                    progress = int(5 * i / len(current_implemented))
+                    print('\r[{}{}]'.format('=' * progress, '.' * (4 - progress)), end='', flush=True)
+                if len(set_implemented) > 0:
+                    output.append('* {}: {}'.format(set_code, '; '.join(markdown_card_link(card_name, set_code, db=db) for card_name in sorted(set_implemented))))
+            if OPTIONS['verbose']:
+                print('\r[ ok ]')
+            if len(output) > 0:
+                copy('\n'.join(output))
+                if OPTIONS['verbose'] and not OPTIONS['stdout']:
+                    print('[ ** ] new cards copied to clipboard')
+            else:
+                if OPTIONS['verbose']:
+                    print('[ ** ] no new cards')
+        finally:
+            subprocess.check_call(['git', 'checkout', 'master'], cwd=str(MASTER), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     elif arguments['markdown-link']:
         if OPTIONS['verbose']:
             print('[....] downloading MTG JSON', end='', flush=True)
